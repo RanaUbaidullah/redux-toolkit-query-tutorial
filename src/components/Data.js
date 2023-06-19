7bf7c2b1268734124959cda80fa2e1f72495abc8
Ubaidullah <researchfigma@gmail.com>
2023-06-19 18:32:59 +0500

    some update

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,37 +1,25 @@
-import {
-  useGetAllProductsQuery,
-  useGetProductQuery,
-} from "../features/apiSlice";
+import {useGetAllProductsQuery,useGetProductQuery,} from "../features/apiSlice";
 import React,{useState} from "react";
-
+import './Data.css'
 export const Data = () => {
   const[search, SetSearch] = useState("")
   function searchp(e){
     SetSearch(e.target.value);
   }
-  const {
-    data: allProductsData,
-    error,
-    isError, 
-    isLoading,
-  } = useGetAllProductsQuery();
+  const {isLoading,} = useGetAllProductsQuery();
   const { data: singleProductData } = useGetProductQuery(search);
-  console.log(allProductsData);
-  console.log(singleProductData);
   if (isLoading) return <h1> Loading...</h1>;
-  return <div>
-     <input onChange={searchp} value={search} type="text" />
-
-    <br/>
-    Data: 
+  return <div>  
+     <input onChange={searchp} value={search} type="text" placeholder="Search" /><br/>
+    <h1>Data: </h1>
     {singleProductData?.products.map((data)=>(
       <div className="card" key={data?.id}>
-          <img src={data.images[0]} alt="Denim Jeans" style={{width:"100%"}}></img>
+      <img src={data.images[0]} alt="Denim Jeans" style={{width:"100%"}}></img>
       <h1>{data?.title}</h1>
-      <p className="price">$19.99</p>
-      <p>Some text about the jeans..</p>
+      <p className="price">${data.price}</p>
+      <p>{data.description}</p>
+      <h5>Category: {data?.category}</h5>
       <p><button>Add to Cart</button></p>
-      {/* <h5>Category: {data?.category}</h5> */}
       </div> 
     ))}
   </div>;
