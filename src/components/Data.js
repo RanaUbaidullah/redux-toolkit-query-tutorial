a5ad459aa9d0606e5e30bf93f9bbfe2391361194
Ubaidullah <researchfigma@gmail.com>
2023-06-19 16:14:19 +0500

    dataupdate

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,27 +1,25 @@
-import {
-  useGetAllProductsQuery,
-  useGetProductQuery,
-} from "../features/apiSlice";
-export const Data = () => {
-  const {
-    data: allProductsData,
-    error,
-    isError,
-    isLoading,
-  } = useGetAllProductsQuery();
-  const { data: singleProductData } = useGetProductQuery("iphone");
-
-  console.log(allProductsData);
-  console.log(singleProductData);
-
-  if (isLoading) return <h1> Loading...</h1>;
-  return <div> Data: 
-    {allProductsData.products.map((data)=>(
-      <>
-      <h1>Title: {data.title}</h1>
-      <h2>Description: {data.description}</h2>
-      <h5>Category: {data.category}</h5>
-      </>
-    ))}
-  </div>;
-};
+import { 
+  useGetAllProductsQuery,  
+  useGetProductQuery, 
+} from "../features/apiSlice";  
+export const Data = () => {  
+  const {  
+    data: allProductsData, 
+    error, 
+    isError, 
+    isLoading, 
+  } = useGetAllProductsQuery(); 
+  const { data: singleProductData } = useGetProductQuery("iphone"); 
+  console.log(allProductsData); 
+  console.log(singleProductData); 
+  if (isLoading) return <h1> Loading...</h1>; 
+  return <div> Data:  
+    {allProductsData.products.map((data)=( 
+      <>   
+      <h1>Title: {data.title}</h1> 
+      <h2>Description: {data.description}</h2> 
+      <h5>Category: {data.category}</h5> 
+      </> 
+    ))} 
+  </div>; 
+}; 
