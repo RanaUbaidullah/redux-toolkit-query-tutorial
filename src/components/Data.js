f60b5f7977257cedf523487b179cebe2c558e1d3
Ubaidullah <researchfigma@gmail.com>
2023-06-19 18:09:11 +0500

    add card imges

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,25 +1,38 @@
-import { 
-  useGetAllProductsQuery,  
-  useGetProductQuery, 
-} from "../features/apiSlice";  
-export const Data = () => {  
-  const {  
-    data: allProductsData, 
-    error, 
+import {
+  useGetAllProductsQuery,
+  useGetProductQuery,
+} from "../features/apiSlice";
+import React,{useState} from "react";
+
+export const Data = () => {
+  const[search, SetSearch] = useState("")
+  function searchp(e){
+    SetSearch(e.target.value);
+  }
+  const {
+    data: allProductsData,
+    error,
     isError, 
-    isLoading, 
-  } = useGetAllProductsQuery(); 
-  const { data: singleProductData } = useGetProductQuery("iphone"); 
-  console.log(allProductsData); 
-  console.log(singleProductData); 
-  if (isLoading) return <h1> Loading...</h1>; 
-  return <div> Data:  
-    {allProductsData.products.map((data)=( 
-      <>   
-      <h1>Title: {data.title}</h1> 
-      <h2>Description: {data.description}</h2> 
-      <h5>Category: {data.category}</h5> 
-      </> 
-    ))} 
-  </div>; 
-}; 
+    isLoading,
+  } = useGetAllProductsQuery();
+  const { data: singleProductData } = useGetProductQuery(search);
+  console.log(allProductsData);
+  console.log(singleProductData);
+  if (isLoading) return <h1> Loading...</h1>;
+  return <div>
+     <input onChange={searchp} value={search} type="text" />
+
+    <br/>
+    Data: 
+    {singleProductData?.products.map((data)=>(
+      <div className="card" key={data?.id}>
+          <img src={data.images[0]} alt="Denim Jeans" style={{width:"100%"}}></img>
+      <h1>{data?.title}</h1>
+      <p className="price">$19.99</p>
+      <p>Some text about the jeans..</p>
+      <p><button>Add to Cart</button></p>
+      {/* <h5>Category: {data?.category}</h5> */}
+      </div> 
+    ))}
+  </div>;
+};
