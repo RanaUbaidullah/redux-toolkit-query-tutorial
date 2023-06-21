89a359b8338bd4dec23f596509f81cc1e00da20e
Ubaidullah <researchfigma@gmail.com>
2023-06-21 12:58:25 +0500

    Update Some packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useEffect } from 'react';
 import "./App.css";
 import { Data } from "./components/Data";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import { ApiProvider } from "@reduxjs/toolkit/query/react";
 import { productsApi } from "./features/apiSlice";
+import {
+  useComponentDidMount,
+  useComponentDidUpdate, 
+  useComponentWillUnmount
+} from "./utils.js";
 
 function App() {
+  useComponentDidMount(() => {
+    console.log("Component did mount!");
+  });
+
+  useComponentDidUpdate(() => {
+    console.log("Component did update!");
+  });
+
+  useComponentWillUnmount(() => {
+    console.log("Component will unmount!");
+  });
   return (
     <Provider store={store}>
       <ApiProvider api={productsApi}>
